1c1e970d220c58e2e2b81281945c8f526526e673
Rawdney Mendes <mendesrawdney@gmail.com>
2024-07-22 13:37:13 -0300

    update file

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,13 +1,39 @@
-import mongoose, { Schema, Document } from 'mongoose';
-
-interface IUser extends Document {
+export interface User {
+  id: number;
   name: string;
   email: string;
 }
 
-const UserSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true }
-});
+export class UserRepository {
+  private users: User[] = [];
+  private nextId: number = 1;
 
-export const User = mongoose.model<IUser>('User', UserSchema);
+  createUser(user: User): User {
+    const newUser = { ...user, id: this.nextId++ };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  getUserById(id: number): User | undefined {
+    return this.users.find(user => user.id === id);
+  }
+
+  getAllUsers(): User[] {
+    return this.users;
+  }
+
+  updateUser(id: number, updatedUser: User): User | undefined {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index !== -1) {
+      this.users[index] = { ...updatedUser, id };
+      return this.users[index];
+    }
+    return undefined;
+  }
+
+  deleteUser(id: number): boolean {
+    const initialLength = this.users.length;
+    this.users = this.users.filter(user => user.id !== id);
+    return this.users.length !== initialLength;
+  }
+}
